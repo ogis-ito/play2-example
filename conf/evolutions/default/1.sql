e72d85cb594561a64fe6f0d9b8f52265d95e75f7
ITO Yoshiichi <Ito_Yoshiichi@ogis-ri.co.jp>
2013-02-11 13:16:42 +0900

    データ変更。

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -14,7 +14,22 @@ create table T_PRODUCT (
   PRICE integer not null
 );
 
+create table T_ORDER (
+  ID bigint not null auto_increment primary key,
+  ORDER_DATE date not null,
+  CUSTOMER_ID bigint not null
+);
+
+create table T_ORDER_DETAIL (
+  ID bigint not null auto_increment primary key,
+  ORDER_ID bigint not null,
+  PRODUCT_ID bigint not null,
+  QUANTITY integer not null
+);
+
 # -- !Downs
 
+drop table T_ORDER_DETAIL;
+drop table T_ORDER;
 drop table T_PRODUCT;
 drop table T_CUSTOMER;
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,20 +2,28 @@
 
 # -- !Ups
 
-create table T_ORDER (
-  ID bigint not null auto_increment primary key,
-  ORDER_DATE date not null,
-  CUSTOMER_ID bigint not null
-);
+insert into T_CUSTOMER (NAME, ADDRESS) values ('鈴木 一郎', '東京都港区');
+insert into T_CUSTOMER (NAME, ADDRESS) values ('山田 花子', '東京都渋谷区');
 
-create table T_ORDER_DETAIL (
-  ID bigint not null auto_increment primary key,
-  ORDER_ID bigint not null,
-  PRODUCT_ID bigint not null,
-  QUANTITY integer not null
-);
+insert into T_PRODUCT (NAME, PRICE) values ('UMLユーザーガイド', 6400);
+insert into T_PRODUCT (NAME, PRICE) values ('UML モデリングのエッセンス', 2400);
+insert into T_PRODUCT (NAME, PRICE) values ('リアルタイムUML', 3800);
+insert into T_PRODUCT (NAME, PRICE) values ('かんたんUML', 1980);
+insert into T_PRODUCT (NAME, PRICE) values ('かんたん！エンタープライズ・アーキテクチャ', 1890);
+insert into T_PRODUCT (NAME, PRICE) values ('オブジェクト開発の真髄', 4200);
+insert into T_PRODUCT (NAME, PRICE) values ('その場で使えるしっかり学べるUML2.0', 2800);
+insert into T_PRODUCT (NAME, PRICE) values ('エンタープライズ統一プロセス', 4500);
+insert into T_PRODUCT (NAME, PRICE) values ('実践！SOAモデリング', 2380);
+insert into T_PRODUCT (NAME, PRICE) values ('オブジェクトデザイン', 3800);
+insert into T_PRODUCT (NAME, PRICE) values ('ThoughtWorksアンソロジー', 2600);
+insert into T_PRODUCT (NAME, PRICE) values ('実践ソフトウェア要求ハンドブック', 2800);
+insert into T_PRODUCT (NAME, PRICE) values ('百年アーキテクチャ', 1500);
+insert into T_PRODUCT (NAME, PRICE) values ('プログラミングScala', 3800);
+insert into T_PRODUCT (NAME, PRICE) values ('実践SysML', 3200);
 
 # -- !Downs
 
-drop table T_ORDER_DETAIL;
-drop table T_ORDER;
+delete from T_ORDER_DETAIL;
+delete from T_ORDER;
+delete from T_PRODUCT;
+delete from T_CUSTOMER;
